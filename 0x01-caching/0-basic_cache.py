7e0c50a21b5bfef0c31a9bce26c77cd8fe716181
R-Owino <sayeakinyi@gmail.com>
2023-10-25 14:37:12 -0700

    [Core]: Create a caching system

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Contains a class BasicCache, that inherits from BaseCaching
+"""
+
+BaseCaching = __import__('BaseClass').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    A caching system that has no limit
+    """
+
+    def put(self, key, item):
+        """
+        Adds an item to the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Gets the cache item by key
+        """
+        return self.cache_data.get(key)
