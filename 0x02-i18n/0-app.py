ac57c8af59b19eea3ca5845c624ffc91d12aff05
R-Owino <sayeakinyi@gmail.com>
2023-11-02 00:00:16 -0700

    Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Simple flask app
+'''
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    ''' Returns a string '''
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000, debug=True)
