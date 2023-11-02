32197565a8fcc2a1bcbf0e94e71e0f4fb6653bde
R-Owino <sayeakinyi@gmail.com>
2023-11-02 02:29:48 -0700

    Force locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -31,10 +31,10 @@ def get_locale() -> str:
     '''
     # Check if the 'locale' parameter is in the request and
     # is a supported locale
-    if request.args.get('locale'):
-        language = request.args.get('locale')
-        if language in app.config['LANGUAGES']:
-            return language
+    if ('locale' in request.args and
+       request.args['locale'] in app.config['LANGUAGES']):
+
+        return request.args['locale']
 
     # else gets the best-matching language from request.accept_languages
     return request.accept_languages.best_match(app.config['LANGUAGES'])
