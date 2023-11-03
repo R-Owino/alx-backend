e8b07ec7872ee209ffc7f7eaf2af8a0ce022d819
R-Owino <sayeakinyi@gmail.com>
2023-11-03 02:09:16 -0700

    Force locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,43 +1,45 @@
 #!/usr/bin/env python3
 '''
-Simple flask app that serves a template
+A simple flask app that serves a template
 '''
-
-from flask import Flask, render_template, request
 from flask_babel import Babel
-
-app = Flask(__name__)
-babel = Babel(app)
+from flask import Flask, render_template, request
 
 
-# config class with languages attribute
-class Config(object):
-    ''' config class for babel '''
+class Config:
+    '''
+    config class for babel
+    '''
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
 @babel.localeselector
 def get_locale() -> str:
+    '''  '''
+    # Check if 'locale' parameter is in the request and is a supported locale
+    if ('locale' in request.args and
+       request.args['locale'] in app.config['LANGUAGES']):
+        return request.args['locale']
+
+    # else get the best-matching language from request.accept_languages
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def get_index() -> str:
     '''
-    Checks URL for locale parameter, and forces the Locale of the app
+    Returns a string
     '''
-    # Check if the 'locale' parameter is in the request and
-    # is a supported locale
-    if request_locale in Config.LANGUAGES:
-        return request_locale
-
-    # else gets the best-matching language from request.accept_languages
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    ''' Returns a string '''
     return render_template('4-index.html')
 
 
-if __name__ == "__main__":
-    app.config.from_object(Config)
-    app.run(host="localhost", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
