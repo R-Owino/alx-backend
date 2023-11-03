223e14faf830253a1e1ec69fb9feb79c6a0607e3
R-Owino <sayeakinyi@gmail.com>
2023-11-03 02:58:49 -0700

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
-'''
-A simple flask app that serves a template
-'''
+''' A simple app that serves a template '''
+
 from flask_babel import Babel
+from typing import Union, Dict
 from flask import Flask, render_template, request, g
 
 
 class Config:
-    '''
-    config class for babel
-    '''
+    ''' Config class for babel '''
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -29,46 +27,32 @@ users = {
 
 def get_user() -> Union[Dict, None]:
     ''' Returns a user dictionary or None if the ID cannot be found '''
-    try:
-        return users.get(int(request.args.get('login_as')))
-    except Exception:
-        return None
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
 
 
 @app.before_request
-def before_request():
+def before_request() -> None:
     ''' Finds a user if any, and set it as a global on flask.g.user '''
     user = get_user()
-    if user:
-        g.user = user
+    g.user = user
 
 
 @babel.localeselector
 def get_locale() -> str:
     ''' Gets specified locale for the web app '''
-    # Check if 'locale' parameter is in the request and is a supported locale
-    query_string = request.query_string.decode('utf-8')
-    query_table = {
-            key: value
-            for key, value in (
-                param.split('=') if '=' in param else (param, '')
-                for param in query_string.split('&')
-            )
-        }
-    if ('locale' in query_table and
-       query_table['locale'] in app.config["LANGUAGES"]):
-        return query_table['locale']
-
-    # else get the best-matching language from request.accept_languages
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
+        return locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
-def get_index() -> str:
-    '''
-    Returns a string
-    '''
-    return render_template('4-index.html')
+def index() -> str:
+    ''' Returns a string '''
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
