25707dba62dd13508daa146ad4d5ba995624ccab
R-Owino <sayeakinyi@gmail.com>
2023-11-03 01:55:20 -0700

    Parameterize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,36 +1,37 @@
 #!/usr/bin/env python3
-'''
-Simple flask app that serves a template
-'''
-
-from flask import Flask, render_template, request
+"""A Basic Flask app with internationalization support.
+"""
 from flask_babel import Babel
-
-app = Flask(__name__)
-babel = Babel(app)
+from flask import Flask, render_template, request
 
 
-# config class with languages attribute
-class Config(object):
-    ''' config class for babel '''
+class Config:
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    ''' Returns a string '''
-    return render_template('3-index.html')
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
-    ''' Determines the best match with our supported languages '''
-    # gets the best-matching language from request.accept_languages
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """Retrieves the locale for a web page.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-if __name__ == "__main__":
-    app.config.from_object(Config)
-    app.run(host="localhost", port=5000, debug=True)
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
