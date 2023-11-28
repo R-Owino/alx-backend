c2e3ac9dc6df31320438e281facb7819d7bc22b8
R-Owino <sayeakinyi@gmail.com>
2023-11-28 12:12:10 +0300

    Chore: Node Redis client and async operations

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,31 @@
+/* Script that connect to the redis server running locally 
+ * function setNewSchool that sets in redis the value for the schoolName
+ * function displaySchoolValue that prints the schoolName value
+ * use callbacks on both functions
+ */
+
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  const asyncGet = promisify(client.get).bind(client);
+  console.log(await asyncGet(schoolName));
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
