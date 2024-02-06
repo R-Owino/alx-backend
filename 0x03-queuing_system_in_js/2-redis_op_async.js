87534b47c86bdd8306512ac568f2d2141b206d73
R-Owino <sayeakinyi@gmail.com>
2024-02-06 23:49:37 +0300

    docs: update the script's docs

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -1,7 +1,7 @@
-/* Script that connect to the redis server running locally 
+/* Script that connect to the redis server running locally
  * function setNewSchool that sets in redis the value for the schoolName
  * function displaySchoolValue that prints the schoolName value
- * use callbacks on both functions
+ * uses async/await and promisify on displaySchoolValue function
  */
 
 import redis from 'redis';
@@ -17,11 +17,11 @@ client.on('error', (err) => {
   console.log(`Redis client not connected to the server: ${err.message}`);
 });
 
-function setNewSchool(schoolName, value) {
+function setNewSchool (schoolName, value) {
   client.set(schoolName, value, redis.print);
 }
 
-async function displaySchoolValue(schoolName) {
+async function displaySchoolValue (schoolName) {
   const asyncGet = promisify(client.get).bind(client);
   console.log(await asyncGet(schoolName));
 }
