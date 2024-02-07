ed40e4f4839a4e6f7ebc89b3fba01c5c3478b276
R-Owino <sayeakinyi@gmail.com>
2024-02-08 00:00:21 +0300

    refactor: ensure a notification is only displayed once

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,25 +1,24 @@
 /* Contains a function createPushNotificationsJobs */
 
-import kue from 'kue';
-const queue = kue.createQueue();
-
-function createPushNotificationsJobs (jobs, queue) {
+function createPushNotificationsJobs(jobs, queue) {
   if (!Array.isArray(jobs)) throw Error('Jobs is not an array');
 
   jobs.forEach(function (job) {
-    const jobCreated = queue.create('push_notification_code_3', job).save((err) => {
-      if (!err) console.log(`Notification job created: ${jobCreated.id}`);
+    const currentJob = queue.create('push_notification_code_3', job);
+
+    // handle job events using 'once' to ensure they are executed only once
+    currentJob.save((err) => {
+      if (!err) console.log(`Notification job created: ${currentJob.id}`);
     });
 
-    // handle job events
-    jobCreated.on('enqueue', () => {
-      console.log(`Notification job created: ${jobCreated.id}`);
-    }).on('complete', (result) => {
-      console.log(`Notification job #${jobCreated.id} completed`);
-    }).on('failed', (errorMessage) => {
-      console.log(`Notification job #${jobCreated.id} failed: ${errorMessage}`);
+    currentJob.once('enqueue', () => {
+      console.log(`Notification job created: ${currentJob.id}`);
+    }).once('complete', (result) => {
+      console.log(`Notification job #${currentJob.id} completed`);
+    }).once('failed', (errorMessage) => {
+      console.log(`Notification job #${currentJob.id} failed: ${errorMessage}`);
     }).on('progress', (progress) => {
-      console.log(`Notification job #${jobCreated.id} ${progress}% complete`);
+      console.log(`Notification job #${currentJob.id} ${progress}% complete`);
     });
   });
 }
